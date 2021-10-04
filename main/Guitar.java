ec22aef57c697940c2c13751811a89846ccf0d9f
FadyAttia11 <fadyattia11@gmail.com>
2021-10-04 02:27:03 +0200

    fix: add specs of guitar to Guitar Constructor

--- main/Guitar.java
+++ main/Guitar.java
@@ -3,10 +3,10 @@ public class Guitar {
     private double price;
     private GuitarSpec spec;
 
-    public Guitar(String serialNumber, double price, GuitarSpec spec) {
+    public Guitar(String serialNumber, double price, Builder builder, String model, Type type, Wood backWood, Wood topWood) {
         this.serialNumber = serialNumber;
         this.price = price;
-        this.spec = spec;
+        this.spec = new GuitarSpec(builder, model, type, backWood, topWood);
     }
 
     public String getSerialNumber() {
